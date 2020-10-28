15e43991d14d43cb939b5fc65d84eb86c92a4ac5
Dmitry Murashov <d.murashov@geoscan.aero>
2020-10-28 17:14:45 +0300

    Minor testkit expansion

--- command.py
+++ command.py
@@ -4,12 +4,28 @@ from command import *
 LUA_PARAM_1_RUN = 1
 LUA_PARAM_1_STOP = 0
 
+
 def msg_lua(param1):
 	msg = mav.command_long_encode(1, 1, mavcommon.MAV_CMD_USER_1, 0, param1, 0, 0, 0, 0, 0, 0)
 	return msg.pack(mav)
 
+
+def msg_arm(arm: bool):
+	msg = mav.command_long_encode(1, 1, mavcommon.MAV_CMD_COMPONENT_ARM_DISARM, 0, int(arm), 0, 0, 0, 0, 0, 0)
+	return msg.pack(mav)
+
+
+def msg_takeoff():
+	msg = mav.command_long_encode(1, 1, mavcommon.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0)
+	return msg.pack(mav)
+
+
 def command_lua_run():
 	send(msg_lua(LUA_PARAM_1_RUN))
-	msgin = wait_for_message(mavcommon.MAVLINK_MSG_ID_COMMAND_ACK)
-	if msgin is not None:
-		print(msgin)
+
+
+def command_arm(arm: bool):
+	send(msg_arm(arm))
+
+def command_takeoff():
+	send(msg_takeoff())
--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from ftp import *
 from command import *
 import serial
+import time
 
 if __name__ == "__main__":
-	ser = serial.Serial('/dev/ttyUSB0', 115200)
-	# msg = mav.vision_position_estimate_encode(1, 2, 3, 4, 5, 6, 7)
-	# ser.write(msg.pack(mav))
-	msg = mav.vision_speed_estimate_encode(1, 2, 3, 4)
-	ser.write(msg.pack(mav))
-	# ftp_write_file(3)
-	# ftp_list()
-	# ftp_write_file()
-	# ftp_read_file()
-	# ftp_list()
-	# command_lua_run()
+	# command_arm(False)
+	command_takeoff()
+	wait_for_message((mavcommon.MAVLINK_MSG_ID_COMMAND_ACK, mavcommon.MAVLINK_MSG_ID_NAMED_VALUE_INT, ), do_print=True, seconds=3)
+	# wait_for_message(mavcommon.MAVLINK_MSG_ID_NAMED_VALUE_INT, do_print=True, seconds=3)
