eb346c99d2751909639e6bbb943269ffde994fe4
Dmitry Murashov <d.murashov@geoscan.aero>
2021-01-18 18:50:27 +0300

    Test FTP upload, finished the script

--- snippets/ftp_upload_lua.py
+++ snippets/ftp_upload_lua.py
@@ -7,6 +7,10 @@ def lua_file_to_bytes(filename="nice_color.lua"):
         return file.read()
 
 
+def _wait_ftp_response(seconds = 10, do_print=False):
+    m = common.wait_for_message((common.mavcommon.MAVLINK_MSG_ID_FILE_TRANSFER_PROTOCOL,), seconds, do_print)
+
+
 def test():
     common.send(ftp.msg_open_file_wo('/main.lua'))
     m = common.wait_for_message((common.mavcommon.MAVLINK_MSG_ID_FILE_TRANSFER_PROTOCOL,), seconds=30)
@@ -15,5 +19,38 @@ def test():
         ftp.FtpPayload.construct_from_bytes(m.get_payload())
 
 
+def open_session(filename='/main.lua') -> int:
+    common.send(ftp.msg_open_file_wo(filename))
+    m = common.wait_for_message((common.mavcommon.MAVLINK_MSG_ID_FILE_TRANSFER_PROTOCOL,), seconds=30)
+
+    if m is not None:
+        m = ftp.FtpPayload.construct_from_bytes(m.get_payload())
+
+    assert m.opcode == ftp.OP_Ack
+    return m.session
+
+
+def write_chunk(sid, offset, chunk: bytearray):
+    # Pack and send
+    msg = ftp.FtpPayload(42, sid, ftp.OP_WriteFile, len(chunk), 0, 0, offset, chunk)
+    msg = ftp.msg_ftp(msg)
+    common.send(msg)
+
+    # Wait response
+    msg = _wait_ftp_response(10)
+    assert msg is not None
+    assert msg.opcode == ftp.OP_Ack
+
+    return
+
+
 if __name__ == "__main__":
-    test()
+    sid = open_session()
+    file = lua_file_to_bytes()
+
+    offset = 0
+    while len(bytes) != 0:
+        chunk = file[:ftp.MAX_Payload]
+        file = file[ftp.MAX_Payload:]
+        write_chunk(sid, offset, chunk)
+        offset += len(chunk)
