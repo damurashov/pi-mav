0851ad4daf317de471446243d2ef6c2b6128d754
Dmitry Murashov <d.murashov@geoscan.aero>
2021-01-27 17:08:30 +0300

    Mic. Parameters: established a 'base' for parameters protocol testing

---
+++ parameters.py
@@ -0,0 +1,18 @@
+# Parameters protocol
+
+from common import send, wait_for_message, mav, mavcommon, mavutil
+
+
+def _msg_param_request_list():
+    msg = mav.param_request_list_encode(1, 1)
+    return msg.pack(mav)
+
+
+def param_request_list():
+    send(_msg_param_request_list())
+
+
+def param_wait_for_param_value(seconds=4, do_print=True):
+    m = wait_for_message((mavcommon.MAVLINK_MSG_ID_PARAM_VALUE,), seconds=seconds, do_print=do_print)
+    if m is not None:
+        print(m)
---
+++ snippets/param_request_list.py
@@ -0,0 +1,7 @@
+from common import wait_for_message
+from parameters import *
+
+
+if __name__ == "__main__":
+    param_request_list()
+    param_wait_for_param_value()
