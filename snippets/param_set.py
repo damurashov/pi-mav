0d992bf44f4d4555884343072e5f4878a6376fd9
Dmitry Murashov <d.murashov@geoscan.aero>
2021-01-28 13:01:34 +0300

    Parameters: PARAM_SET, minor amendments to the respective snippet

--- snippets/param_set.py
+++ snippets/param_set.py
@@ -1,8 +1,8 @@
 from parameters import *
 
 if __name__ == "__main__":
-    param_set("BrdNmr", 500.0)
-    param_wait_for_param_value()
+    param_set("BrdNmr", 501.0)
+    param_wait_for_param_value(1)
 
     param_request_read(param_id='BrdNmr')
-    param_wait_for_param_value()
+    param_wait_for_param_value(1)
