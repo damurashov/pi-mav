339a63816702946c07772c2fb4dc03305b1a3300
Dmitry Murashov <d.murashov@geoscan.aero>
2021-01-28 12:20:52 +0300

    Parameters: PARAM_SET

--- parameters.py
+++ parameters.py
@@ -8,13 +8,23 @@ def _msg_param_request_list():
     return msg.pack(mav)
 
 
+def _encode_param_id(param_id: str = ''):
+    param_id = bytearray(param_id[0:16], encoding='ascii')
+    return param_id
+
+
 def _msg_param_request_read(param_index=-1, param_id=''):
     if param_index < 0 and len(param_id) == 0:
         raise ValueError()
 
-    param_id = bytearray(param_id[0:16], encoding='ascii')
-    print(param_id)
+    param_id = _encode_param_id(param_id)
     msg = mav.param_request_read_encode(1, 1, param_id, param_index)
+    return msg.pack(mav)
+
+
+def _msg_param_set(param_id: str, param_value: float):
+    param_id = _encode_param_id(param_id)
+    msg = mav.param_set_encode(1, 1, param_id, param_value, mavcommon.MAVLINK_TYPE_FLOAT)
     return msg.pack(mav)
 
 
@@ -24,6 +34,10 @@ def param_request_list():
 
 def param_request_read(param_index=-1, param_id=''):
     send(_msg_param_request_read(param_index, param_id))
+
+
+def param_set(param_id: str, param_value: float):
+    send(_msg_param_set(param_id, param_value))
 
 
 def param_wait_for_param_value(seconds=4, do_print=True):
