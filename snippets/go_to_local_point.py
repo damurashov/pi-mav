9826053c64479df252c4bbe8ee7ca8b0ade394f4
Dmitry Murashov <d.murashov@geoscan.aero>
2021-03-01 18:01:20 +0300

    [snippets] goToLocalPoint

---
+++ snippets/go_to_local_point.py
@@ -0,0 +1,10 @@
+from generic import *
+
+
+def _wait_response(seconds=4, do_print=True):
+	wait_for_message((mavcommon.MAVLINK_MSG_ID_POSITION_TARGET_LOCAL_NED,), do_print=do_print, seconds=seconds)
+
+
+if __name__ == "__main__":
+	set_position_target_local_ned()
+	_wait_response()
