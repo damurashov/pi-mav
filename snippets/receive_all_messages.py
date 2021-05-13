fef3a66542eb48d515eca8ab3b6aea2621dd0d1e
Dmitry Murashov <d.murashov@geoscan.aero>
2021-05-13 15:32:49 +0300

    [snippets/receive_all_messages] Implemented receiving of all messages for connection quality assessment

---
+++ snippets/receive_all_messages.py
@@ -0,0 +1,8 @@
+from connectivity import wait_for_message, send
+from connectivity import mavcommon
+
+if __name__ == "__main__":
+	while True:
+		send(b'geoscan')
+		wait_for_message((mavcommon.MAVLINK_MSG_ID_HEARTBEAT,), seconds=1, do_print=True)
+		# wait_for_message(None, seconds=1, do_print=True)
