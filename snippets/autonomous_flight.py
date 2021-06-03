572b7be7caf1031db6e75e1a1b3c91b77f85b6bd
Dmitry Murashov <d.murashov@geoscan.aero>
2021-06-03 16:49:58 +0300

    [snippets] Added "autonomous_flight" snippet | Test's AP's basic functionality related to autonomous flight (takeoff, go to local point)

---
+++ snippets/autonomous_flight.py
@@ -0,0 +1,23 @@
+from command import *
+from generic import set_position_target_local_ned
+
+if __name__ == "__main__":
+	print('arm')
+	command_arm(True)
+	time.sleep(2)
+
+	print('takeoff')
+	command_takeoff()
+	time.sleep(10)
+
+	print('go to local point')
+	set_position_target_local_ned()
+	time.sleep(10)
+
+	print('go to local point')
+	set_position_target_local_ned()
+	time.sleep(10)
+
+	print('land')
+	command_land()
+	time.sleep(10)
