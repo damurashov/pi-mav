aee6b848ce23ca8560e1a2b9fbb46bf1eb54d161
DmitryMurashov <dmtr.murashov@gmail.com>
2022-02-28 15:08:26 +0300

    [ex*:gs_net*connect] Impl | Various testing code

--- examples/gs_network_open_connect.py
+++ examples/gs_network_open_connect.py
@@ -27,12 +27,38 @@ def send_tcp(gs_network):
 	print(port)
 
 
+def open_close_udp(gs_network):
+	gs_network.open_udp4(2222)
+	gs_network.close_udp4(2222)
+
+
+def send_udp(gs_network):
+	# port, ack = gs_network.send_udp(b"Echo", REMOTE_ENDPOINT_IP, REMOTE_ENDPOINT_PORT, 2222)
+	# assert 2222 == int(port)
+	# ack = gs_network.close_udp4(2222)
+
+	port, ack = gs_network.send_udp(b"Hello", REMOTE_ENDPOINT_IP, REMOTE_ENDPOINT_PORT)
+	gs_network.close_udp4(port)
+
+
+def get_process_received(gs_network):
+	timeout_wait_seconds = 30000
+	gs_network.open_tcp4(8989)
+	ret = gs_network.wait_process_received(timeout_wait_seconds)
+	gs_network.close_tcp4(8989)
+
+	print(ret)
+
+
 if __name__ == "__main__":
 	connection = MavlinkConnection.build_connection(MavlinkConnection.PROFILE_UDP)
 	gs_network = gsnet.GsNetwork(connection, FORCE_RESPONSE)
-	switch = 0
+	switch = 4
 
 	{
 		0: open_close_tcp,
 		1: send_tcp,
+		2: open_close_udp,
+		3: send_udp,
+		4: get_process_received,
 	}[switch](gs_network)
