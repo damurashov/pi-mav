4ccc34ac91e739d8239640243f57d22035152fbb
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-25 15:26:43 +0300

    [wrapper:camera] Impl | camera*capture

--- src/wrapper/camera.py
+++ src/wrapper/camera.py
@@ -32,4 +32,41 @@ class Camera:
 		self.mavlink_connection.mav.send(message_request)
 
 	def wait_camera_information(self, block=True, timeout_seconds=None):
-		pass
+		"""
+		:return: Returns either `COMMAND_ACK` w/ error code, or `CAMERA_INFORMATION`.
+
+		According to the standard, both
+		messages are expected, but in case of successfully acquiring the `CAMERA_INFORMATION` message waiting for
+		possibly skipped or reordered `COMMAND_ACK` makes no difference or sense.
+		"""
+		for i in range(3):  # https://mavlink.io/en/services/camera.html#camera-connection, we have to make 3 attempts
+			message = self.mavlink_connection.recv_match(type=["COMMAND_ACK", "CAMERA_INFORMATION"],
+				blocking=block, timeout=timeout_seconds)
+
+			if message.id == common.MAVLINK_MSG_ID_CAMERA_INFORMATION:
+				break
+			elif message.id == common.MAVLINK_MSG_ID_COMMAND_ACK:
+				if message.result != common.MAV_RESULT_ACCEPTED and message.command == common.MAV_CMD_REQUEST_MESSAGE:  # On our message request, we've received a response telling us that the request cannot be fullfilled
+					break
+
+		return message
+
+	def send_cmd_image_start_capture_once(self):
+		interval = 0
+		total_images = 1
+		sequence_number = 0
+		message_start_capture = self.mavlink_connection.mav.command_long_encode(1, 100,
+			common.MAV_CMD_IMAGE_START_CAPTURE, 0, interval, total_images, sequence_number, float('nan'), float('nan'),
+			float('nan'))
+		self.mavlink_connection.mav.send(message_start_capture)
+
+	def wait_camera_image_captured(self, block=True, timeout_seconds=None):
+		for i in range(3):
+			message = self.mavlink_connection.recv_match(type=["COMMAND_ACK", "CAMERA_IMAGE_CAPTURED"],
+				blocking=block, timeout=timeout_seconds)
+
+			if message.id == common.MAVLINK_MSG_ID_CAMERA_IMAGE_CAPTURED:
+				break
+			elif message.id == common.MAVLINK_MSG_ID_COMMAND_ACK:
+				if message.result != common.MAV_RESULT_ACCEPTED and message.command == common.MAV_CMD_REQUEST_MESSAGE:  # On our message request, we've received a response telling us that the request cannot be fullfilled
+					break
