7925bdc65e0ddaa3cc3696df4c6bd1675ddd50ed
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-16 10:38:30 +0300

    [examples:rc_channels] Impl

---
+++ examples/rc_channels.py
@@ -0,0 +1,21 @@
+import pathlib
+import sys
+
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "src"))  # We need files from "src/", that's how we access them
+
+from connectivity import MavlinkConnection
+
+
+if __name__ == "__main__":
+	device_serial = sys.argv[1]
+	baudrate = sys.argv[2]
+	connection = MavlinkConnection.build_connection(MavlinkConnection.PROFILE_SERIAL, serial=device_serial, baudrate=baudrate)
+
+	roll = 1500
+	pitch = 1500
+	yaw = 1500
+	throttle = 1500
+	mode = 1
+
+	while True:
+		connection.mav.rc_channels_override_send(1, 1, throttle, yaw, pitch, roll, mode, 0, 0, 0)
