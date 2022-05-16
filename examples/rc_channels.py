00d490b573c2af04ccc20699121c442c4ae7ffc6
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-16 10:39:23 +0300

    [examples:rc_channels] Enh | Delay

--- examples/rc_channels.py
+++ examples/rc_channels.py
@@ -1,5 +1,6 @@
 import pathlib
 import sys
+import time
 
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "src"))  # We need files from "src/", that's how we access them
 
@@ -18,4 +19,6 @@ if __name__ == "__main__":
 	mode = 1
 
 	while True:
+		sleep_period_seconds = .2
 		connection.mav.rc_channels_override_send(1, 1, throttle, yaw, pitch, roll, mode, 0, 0, 0)
+		time.sleep(sleep_period_seconds)
