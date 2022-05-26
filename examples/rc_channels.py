481697d3b80379f7979d198fd8e502ffc99d3795
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-26 17:31:37 +0300

    [examples:rc_channels] Ref | Magic numbers

--- examples/rc_channels.py
+++ examples/rc_channels.py
@@ -16,9 +16,12 @@ if __name__ == "__main__":
 	pitch = 1500
 	yaw = 1500
 	throttle = 1500
-	mode = 1
+	mode = 1000
+	mode1 = 1500
+	flaps = 1000
+	parachute = 1000
 
 	while True:
-		sleep_period_seconds = .2
-		connection.mav.rc_channels_override_send(1, 1, throttle, yaw, pitch, roll, mode, 0, 0, 0)
+		sleep_period_seconds = .02
+		connection.mav.rc_channels_override_send(1, 1, throttle, yaw, pitch, roll, mode, mode1, flaps, parachute)
 		time.sleep(sleep_period_seconds)
