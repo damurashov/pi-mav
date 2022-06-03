35dcabe145af4167c46a0197fbdbb111605028f3
DmitryMurashov <dmtr.murashov@gmail.com>
2022-06-03 16:35:28 +0300

    [examples:led] Bug | Missing imports

--- examples/led.py
+++ examples/led.py
@@ -5,12 +5,13 @@ import time
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "src"))  # We need files from "src/", that's how we access them
 
 from connectivity import MavlinkConnection
+from pymavlink.dialects.v20 import common
 
 
 if __name__ == "__main__":
 	connection = MavlinkConnection.build_connection(MavlinkConnection.PROFILE_UDP)
 
-	index = 1
+	index = 0
 	r = 1
 	g = 1
 	b = 1
