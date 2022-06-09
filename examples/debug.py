ca84dfa424cfe2a458015a379b13ea7a409bf4d0
DmitryMurashov <dmtr.murashov@gmail.com>
2022-06-09 11:14:21 +0300

    [examples:debug] Enh

--- examples/debug.py
+++ examples/debug.py
@@ -9,9 +9,17 @@ import time
 sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent / "src"))  # We need files from "src/", that's how we access them
 
 from connectivity import MavlinkConnection
+from wrapper.cmd_nav import CmdNav
+from wrapper.control import Control
 
 
 if __name__ == "__main__":
 	connection = MavlinkConnection.build_connection(MavlinkConnection.PROFILE_UDP)
 	debug = connection.mav.debug_encode(0, 0, 0)
+	cmdnav = CmdNav(connection, 1, 1)
+	ctl = Control(connection, 1, 1)
 	connection.mav.send(debug)
+
+	while True:
+		print(ctl.recv_mission_item_reached())
+		connection.mav.send(debug)
